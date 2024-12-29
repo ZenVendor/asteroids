b2270af707074dce17a368a3d4f46d6806c4aea3
Zen Vendor <zen.vendor@mixbox.pl>
2024-12-29 22:43:34 +0100

    constants

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import constants as c
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {c.SCREEN_WIDTH}")
+    print(f"Screen height: {c.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
