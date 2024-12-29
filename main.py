357a37315bbe2f0b7e49878cb345541ea5434c10
Zen Vendor <zen.vendor@mixbox.pl>
2024-12-29 23:28:40 +0100

    draw player

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 import constants as c
+import player
 
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    ship = player.Player(c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+        ship.draw(screen)
+
+        clock.tick(60)
+        dt = clock.get_time() / 1000
         pygame.display.flip()
 
 
